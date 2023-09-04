f04393624e37ec3d14c22a65482a8f1871aba1ac
Yago Iglesias <yagoiglesias7@gmail.com>
2023-09-04 14:37:29 +0200

    test: add tests for filter

--- src/filter.rs
+++ src/filter.rs
@@ -29,11 +29,11 @@ impl Filter {
                 url
             }
             Filter::Exists(attribute, exists) => {
-                let mut url = String::from(attribute);
-                url.push('=');
+                let mut url = String::new();
                 if !exists {
-                    url.push('?');
+                    url.push('!');
                 }
+                url.push_str(attribute);
                 url
             }
         }
@@ -58,6 +58,124 @@ impl Operation {
             Operation::Lt => "<",
             Operation::Gte => ">=",
             Operation::Lte => "<=",
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Attribute, BookAttribute, Filter, MovieAttribute, Operation};
+
+    #[test]
+    fn test_match_eq() {
+        let filter_eq = Filter::Match(
+            Attribute::Book(BookAttribute::Name),
+            Operation::Eq,
+            vec!["The Fellowship of the Ring".to_string()],
+        );
+        assert_eq!(
+            filter_eq.get_url(),
+            "name=The Fellowship of the Ring".to_string()
+        );
+    }
+
+    #[test]
+    fn test_match_ne() {
+        let filter_ne = Filter::Match(
+            Attribute::Book(BookAttribute::Name),
+            Operation::Ne,
+            vec!["The Fellowship of the Ring".to_string()],
+        );
+        assert_eq!(
+            filter_ne.get_url(),
+            "name!=The Fellowship of the Ring".to_string()
+        );
+    }
+
+    #[test]
+    fn test_exists() {
+        let filter_include = Filter::Exists("name".to_string(), true);
+        assert_eq!(filter_include.get_url(), "name".to_string());
+    }
+
+    #[test]
+    fn test_dont_exist() {
+        let filter_exclude = Filter::Exists("name".to_string(), false);
+        assert_eq!(filter_exclude.get_url(), "!name".to_string());
+    }
+
+    #[test]
+    fn test_include_and_exclude() {
+        let filter = Filter::Match(
+            Attribute::Book(BookAttribute::Name),
+            Operation::Eq,
+            vec![
+                "The Fellowship Of The Ring".to_string(),
+                "The Two Towers".to_string(),
+                "The Return Of The King".to_string(),
+            ],
+        );
+
+        assert_eq!(
+            filter.get_url(),
+            "name=The Fellowship Of The Ring,The Two Towers,The Return Of The King".to_string()
+        );
+
+        let filter = Filter::Match(
+            Attribute::Book(BookAttribute::Name),
+            Operation::Ne,
+            vec![
+                "The Fellowship Of The Ring".to_string(),
+                "The Two Towers".to_string(),
+                "The Return Of The King".to_string(),
+            ],
+        );
+
+        assert_eq!(
+            filter.get_url(),
+            "name!=The Fellowship Of The Ring,The Two Towers,The Return Of The King".to_string()
+        );
+    }
+
+    #[test]
+    fn test_operations() {
+        let tests = vec![
+            (
+                Filter::Match(
+                    Attribute::Movie(MovieAttribute::BudgetInMillions),
+                    Operation::Gt,
+                    vec!["10".to_string()],
+                ),
+                "budgetInMillions>10",
+            ),
+            (
+                Filter::Match(
+                    Attribute::Movie(MovieAttribute::BudgetInMillions),
+                    Operation::Gte,
+                    vec!["10".to_string()],
+                ),
+                "budgetInMillions>=10",
+            ),
+            (
+                Filter::Match(
+                    Attribute::Movie(MovieAttribute::BudgetInMillions),
+                    Operation::Lt,
+                    vec!["10".to_string()],
+                ),
+                "budgetInMillions<10",
+            ),
+            (
+                Filter::Match(
+                    Attribute::Movie(MovieAttribute::BudgetInMillions),
+                    Operation::Lte,
+                    vec!["10".to_string()],
+                ),
+                "budgetInMillions<=10",
+            ),
+        ];
+
+        for (filter, expected) in tests {
+            assert_eq!(filter.get_url(), expected.to_string());
         }
     }
 }
