5a2b1685b852152a71963aed0b3f268cf97f88ff
Yago Iglesias <yagoiglesias7@gmail.com>
2023-09-05 17:31:23 +0200

    chore: Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub use item::Item;
 pub use item::ItemType;
 pub use request::filter::Filter;
 pub use request::filter::Operator;
+pub use request::pagination;
 pub use request::sort::Sort;
 pub use request::sort::SortOrder;
-pub use request::pagination;
 pub use request::Request;
